606156b6d23b1405117ce463b048b2aa8ab66ccf
alahi sahi <alahisahi20@gmail.com>
2025-04-07 23:37:17 +0530

    7-4-2025

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 import findspark
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType,DoubleType
 findspark.init()
 
 
@@ -9,7 +10,18 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Create a simple DataFrame
-df=spark.read.csv(r"C:\Users\alahi\Downloads\SampleCSVFile_11kb.csv",  inferSchema=True)
+schema = StructType([
+    StructField("Id", IntegerType(), True),
+    StructField("Address", StringType(), True),
+    StructField("Name", StringType(), True),
+    StructField("qty", IntegerType(), True),
+    StructField("Anount", DoubleType(), True),
+    StructField("Unitprice", DoubleType(), True),
+    StructField("Place", StringType(), True),
+    StructField("Type", StringType(), True),
+    StructField("power", DoubleType(), True),
+])
+df=spark.read.csv(r"C:\Users\alahi\Downloads\SampleCSVFile_11kb.csv",  schema=schema, header=True)
 
 # Show the DataFrame
 print(df.printSchema())
